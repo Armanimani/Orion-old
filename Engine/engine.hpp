c45794b6c779dc19f6da532d88308cf748dddb44
Arman Imani <armanimani87@gmail.com>
2020-07-26 16:18:30 +0200

    Added a new macro for asserting a condition in debug mode

--- Engine/engine.hpp
+++ Engine/engine.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cassert>
+
 #ifdef ORION_BUILD_DLL
 	#define ORION_API __declspec(dllexport)
 #else
@@ -8,6 +10,12 @@
 
 #ifdef _DEBUG
 	#define ORION_DEBUG
+#endif
+
+#ifdef ORION_DEBUG
+	#define ORION_ASSERT(Expression) assert(Expression)
+#else
+	#define ORION_ASSERT(Expression) ;
 #endif
 
 #include "Engine/Common/base_types.hpp"
