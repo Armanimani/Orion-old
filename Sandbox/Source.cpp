65bc0b0962152ec7b1c3d2667f5fd1a8ffabce58
Arman Imani <armanimani87@gmail.com>
2020-07-27 23:56:54 +0200

    Added the sandbox project main file to test the logging ssytem internally

---
+++ Sandbox/Source.cpp
@@ -0,0 +1,14 @@
+#include "Engine/Logging/ConsoleLogger.hpp"
+
+int main()
+{
+	orion::ConsoleLogger::enableLoggingLevel(orion::LoggingLevel::critical);
+	orion::ConsoleLogger::enableLoggingLevel(orion::LoggingLevel::warning);
+	orion::ConsoleLogger::enableLoggingLevel(orion::LoggingLevel::information);
+	orion::ConsoleLogger::enableLoggingLevel(orion::LoggingLevel::debug);
+
+	orion::ConsoleLogger::logCritical("Sandbox", "Critical message example");
+	orion::ConsoleLogger::logWarning("Sandbox", "Warning message example");
+	orion::ConsoleLogger::logDebug("Sandbox", "Debug message example");
+	orion::ConsoleLogger::logInformation("Sandbox", "Information message example");
+}
