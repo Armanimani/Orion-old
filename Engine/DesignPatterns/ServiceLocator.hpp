be4225197dce7eba444cc12b201606c557a51b0f
Arman Imani <armanimani87@gmail.com>
2020-07-27 23:56:23 +0200

    Added a service locator design patter that can be used to provide/get services

---
+++ Engine/DesignPatterns/ServiceLocator.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <memory>
+
+namespace orion
+{
+	template<typename IService>
+	class ServiceLocator
+	{
+	public:
+		static void provide(std::unique_ptr<IService>&& service) noexcept;
+		static IService* get() noexcept;
+	private:
+		inline static std::unique_ptr<IService> s_service {nullptr};
+	};
+
+
+	template <typename IService>
+	void ServiceLocator<IService>::provide(std::unique_ptr<IService>&& service) noexcept
+	{
+		if (s_service)
+			s_service.reset();
+
+		s_service = std::move(service);
+	}
+
+	template <typename IService>
+	IService* ServiceLocator<IService>::get() noexcept
+	{
+		ORION_ASSERT(s_service);
+
+		return s_service.get();
+	}
+}
